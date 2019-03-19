73d14d252cb89fa83d1d90a3c8166047b779709e
David <dhahn123@gmail.com>
2019-03-19 15:03:21 -0700

    test

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ module.exports = {
   ],
   author: {
     name: 'SurgTime Inc',
-    photo: 'static/media/surgtime-logo.png',
+    photo: 'static/media/raw-food.jpg',
     bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
     contacts: {
       email: '#',
