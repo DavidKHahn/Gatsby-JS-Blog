7e6f23aba4379a97fd3d3229b5466acb518f48e1
David <dhahn123@gmail.com>
2019-03-19 14:41:39 -0700

    minor fix to location of png

--- gatsby-config.js
+++ gatsby-config.js
@@ -159,7 +159,7 @@ module.exports = {
         background_color: '#FFF',
         theme_color: '#F7A046',
         display: 'standalone',
-        icon: 'static/surgtime-logo.png'
+        icon: '/surgtime-logo.png'
       },
     },
     'gatsby-plugin-offline',
